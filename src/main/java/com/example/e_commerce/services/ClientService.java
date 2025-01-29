0177ee9f45bdc46f77ce19507e32a601d7dd001b
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-29 15:51:00 -0300

    feat: changes class name ClientServices to ClientService.

---
+++ src/main/java/com/example/e_commerce/services/ClientService.java
@@ -0,0 +1,34 @@
+package com.example.e_commerce.services;
+
+import com.example.e_commerce.dtos.ClientRequestDTO;
+import com.example.e_commerce.dtos.ClientResponseDTO;
+import com.example.e_commerce.models.Client;
+import com.example.e_commerce.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServices {
+
+    @Autowired
+    private final ClientRepository clientRepository;
+
+    public ClientServices(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public ClientResponseDTO criarCliente(ClientRequestDTO clientRequestDTO) {
+        Client client = new Client(
+                clientRequestDTO.getName(),
+                clientRequestDTO.getCpf(),
+                clientRequestDTO.getEmail()
+        );
+        Client clientSave = clientRepository.save(client);
+        return new ClientResponseDTO(
+                clientSave.getId(),
+                clientSave.getName(),
+                clientSave.getCpf(),
+                clientSave.getEmail()
+        );
+    }
+}
