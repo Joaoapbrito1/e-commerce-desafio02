dd2a78afcc71b2c7d13c949ede16d89f46b48132
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-29 14:09:09 -0300

    feat: implements the ClientResponseDTO class.

--- src/main/java/com/example/e_commerce/dtos/ClientResponseDTO.java
+++ src/main/java/com/example/e_commerce/dtos/ClientResponseDTO.java
@@ -1,4 +1,32 @@
 package com.example.e_commerce.dtos;
 
-public class ClientDTO {
+
+public class ClientResponseDTO {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
