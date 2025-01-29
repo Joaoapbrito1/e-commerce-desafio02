6fd9be80f6a9883474e4f1d471f5aa763269569e
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-29 10:54:42 -0300

    feat: implement the client class.

--- src/main/java/com/example/e_commerce/models/Client.java
+++ src/main/java/com/example/e_commerce/models/Client.java
@@ -1,4 +1,53 @@
 package com.example.e_commerce.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String email;
+
+    public Client(){
+    }
+    public Client(Long id, String name, String cpf, String email){
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email= email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public String getEmail() {
+        return email;
+    }
 }
