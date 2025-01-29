97db5821f34203fab0b5aa45beebe9bdadb19b8f
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-29 16:20:36 -0300

    feat: creates endpoint for client registrations in the ClientController class.

--- src/main/java/com/example/e_commerce/controllers/ClientController.java
+++ src/main/java/com/example/e_commerce/controllers/ClientController.java
@@ -1,4 +1,29 @@
 package com.example.e_commerce.controllers;
 
+
+import com.example.e_commerce.dtos.ClientRequestDTO;
+import com.example.e_commerce.dtos.ClientResponseDTO;
+import com.example.e_commerce.models.Client;
+import com.example.e_commerce.services.ClientService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/client")
 public class ClientController {
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientResponseDTO> createClient(@Valid @RequestBody ClientRequestDTO clienteRequestDTO) {
+        ClientResponseDTO clientResponseDTO = clientService.createClient(clienteRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientResponseDTO);
+    }
 }
