71b483ce31216d43bce81764705e9d60894c9bdc
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-30 17:14:04 -0300

    refactor: add this.id to the constructor of the ProductResponseDTO class.

--- src/main/java/com/example/e_commerce/dtos/ProductResponseDTO.java
+++ src/main/java/com/example/e_commerce/dtos/ProductResponseDTO.java
@@ -8,6 +8,7 @@ public class ProductResponseDTO {
     private int quantity;
 
     public ProductResponseDTO(Long id,String name, double price, int quantity) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
