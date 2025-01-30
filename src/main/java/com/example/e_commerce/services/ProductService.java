accbcc70c6e3832bc58de1e7c8e59ba14e6b9f6a
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-30 16:44:11 -0300

    feat: implements the ProductService class and the createProduct method.

--- src/main/java/com/example/e_commerce/services/ProductService.java
+++ src/main/java/com/example/e_commerce/services/ProductService.java
@@ -1,4 +1,30 @@
 package com.example.e_commerce.services;
 
+import com.example.e_commerce.dtos.ProductRequestDTO;
+import com.example.e_commerce.dtos.ProductResponseDTO;
+import com.example.e_commerce.models.Product;
+import com.example.e_commerce.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public ProductResponseDTO createProduct(ProductRequestDTO productRequestDTO) {
+        Product product = new Product(
+                productRequestDTO.getName(),
+                productRequestDTO.getPrice(),
+                productRequestDTO.getQuantity()
+        );
+        Product productSave = productRepository.save(product);
+        return new ProductResponseDTO(
+                product.getId(),
+                product.getName(),
+                product.getPrice(),
+                product.getQuantity()
+        );
+    }
 }
