9b34669e0a52ebbf2f595f09980e6ba775e2a11b
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-30 17:11:55 -0300

    feat: implements the createProduct endpoint in the ProductController class.

--- src/main/java/com/example/e_commerce/controllers/ProductController.java
+++ src/main/java/com/example/e_commerce/controllers/ProductController.java
@@ -1,4 +1,29 @@
 package com.example.e_commerce.controllers;
 
+import com.example.e_commerce.dtos.ProductRequestDTO;
+import com.example.e_commerce.dtos.ProductResponseDTO;
+import com.example.e_commerce.services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> createProduct(@Valid @RequestBody ProductRequestDTO productRequestDTO){
+        ProductResponseDTO productResponseDTO = productService.createProduct(productRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResponseDTO);
+    }
 }
