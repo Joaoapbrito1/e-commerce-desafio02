96cf4fba0ed99a543e5e6855757a325d04fe8315
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-30 16:10:55 -0300

    feat: implements the ProductRepository class.

--- src/main/java/com/example/e_commerce/repositories/ProductRepository.java
+++ src/main/java/com/example/e_commerce/repositories/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.example.e_commerce.repositories;
 
-public interface ProductRepository {
+import com.example.e_commerce.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
