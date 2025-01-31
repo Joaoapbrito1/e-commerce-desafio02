6e4b60e8056bbf2b6d9a24af32fe9b555dae0a60
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-31 15:37:17 -0300

    feat: implements PurchaseController class.

--- src/main/java/com/example/e_commerce/controllers/PurchaseController.java
+++ src/main/java/com/example/e_commerce/controllers/PurchaseController.java
@@ -1,4 +1,21 @@
 package com.example.e_commerce.controllers;
 
+import com.example.e_commerce.services.PurchaseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/purchases")
 public class PurchaseController {
+    private final PurchaseService purchaseService;
+
+    public PurchaseController(PurchaseService purchaseService) {
+        this.purchaseService = purchaseService;
+    }
+
+    @PostMapping("/{productId}")
+    public ResponseEntity<String> purchaseProduct(@PathVariable Long productId) {
+        purchaseService.purchaseProduct(productId);
+        return ResponseEntity.ok("Purchase successful");
+    }
 }
