7c0fdb42c903b10aa422ebab09f69f82b1f29cc4
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-31 15:36:39 -0300

    feat: implements PurchaseService class.

--- src/main/java/com/example/e_commerce/services/PurchaseService.java
+++ src/main/java/com/example/e_commerce/services/PurchaseService.java
@@ -1,4 +1,28 @@
 package com.example.e_commerce.services;
 
+import com.example.e_commerce.models.Product;
+import com.example.e_commerce.repositories.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
 public class PurchaseService {
+    private final ProductRepository productRepository;
+
+    public PurchaseService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void purchaseProduct(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+
+        if (product.getQuantity() <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product out of stock");
+        }
+
+        product.setQuantity(product.getQuantity() - 1);
+        productRepository.save(product);
+    }
 }
