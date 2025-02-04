65e84533c845ec0221e8e5b793a55174d70af463
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-04 12:00:04 -0300

    Revert "Feat/customer"

--- src/main/java/com/example/e_commerce/exceptions/InvalidPurchaseException.java
+++
@@ -1,7 +0,0 @@
-package com.example.e_commerce.exceptions;
-
-public class InvalidPurchaseException extends RuntimeException {
-    public InvalidPurchaseException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/e_commerce/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.e_commerce.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
